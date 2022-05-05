b16c1845284e324f75b2c75e884849f750ca4108
maxbrookerr <78735324+maxbrookerr@users.noreply.github.com>
2022-05-05 16:18:14 +0100

    final

---
+++ Assets/DOOR STUFF/SoundFX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFX : MonoBehaviour
+{
+    public AudioSource playSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+}
